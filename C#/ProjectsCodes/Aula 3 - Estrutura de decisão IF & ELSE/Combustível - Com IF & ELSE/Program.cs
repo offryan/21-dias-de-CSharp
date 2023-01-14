552be70aee998e074996c2004dfa8c279a735fea
offryan <ryan.gsantos3@gmail.com>
2023-01-14 10:12:17 -0800

    IF & ELSE - 14/01

---
+++ C#/ProjectsCodes/Aula 3 - Estrutura de decisão IF & ELSE/Combustível - Com IF & ELSE/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("=============================POSTO================================ ");
+
+Console.WriteLine("Digite a quantidade de litros disponíveis de combustível no posto: ");
+double? estoque = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a cotação em reais por litro: ");
+double? valorLitros = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("============================CLIENTE=============================== ");
+
+Console.WriteLine("Digite seu nome: ");
+String? nomeCliente = (Console.ReadLine());
+
+Console.WriteLine($"Olá, seja bem vindo {nomeCliente}. Digite a quantidade de litros desejado: ");
+double? qtdCombustivel = Convert.ToDouble(Console.ReadLine());
+
+if (qtdCombustivel > estoque){
+    Console.WriteLine($"Olá, {nomeCliente}. A quantidade solicitada {qtdCombustivel} litros é maior que a disponível em nosso estoque atual, que é de: {estoque} LTS");
+}
+else{
+Console.WriteLine("============================LÓGICA================================ =");
+
+    double? valorResultante = valorLitros * qtdCombustivel;
+    double? finalEstoque = estoque - qtdCombustivel;
+    double? combustivelCliente = qtdCombustivel / valorLitros;
+
+Console.WriteLine("============================SAÍDA================================= =");
+
+Console.WriteLine(
+$"""
+    {nomeCliente} você colocou: {qtdCombustivel} LTS, e o valor total foi de R$: {valorResultante},00.
+
+    Estoque atual do posto é de: {finalEstoque} LTS
+""");
+
+Console.WriteLine("================================================================== ");
+}
---
+++ C#/ProjectsCodes/Aula 3 - Estrutura de decisão IF & ELSE/Combustível - SEM IF & ELSE/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Digite a qtd de litros de combustível: ");
+int? QtdCombustivel = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o valor do combustivel em R$: ");
+int? valorCombustivel = Convert.ToInt32(Console.ReadLine());
+
+int? estoque = 8000;
+
+int? ResEstoque = estoque - QtdCombustivel;
+
+Console.WriteLine(
+$"""
+    Você colocou: {QtdCombustivel} LTS, e o valor foi de R$: {valorCombustivel}
+    Estoque atual do posto de:  {ResEstoque} LTS
+""");
