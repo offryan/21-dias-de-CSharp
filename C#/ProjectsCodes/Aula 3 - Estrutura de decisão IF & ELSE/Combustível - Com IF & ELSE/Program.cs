25eb5ee6c2d10c898c5877a0e820feb8f7168005
offryan <ryan.gsantos3@gmail.com>
2023-01-17 09:55:25 -0300

    aula 4

--- C#/ProjectsCodes/Aula 3 - Estrutura de decisão IF & ELSE/Combustível - Com IF & ELSE/Program.cs
+++ C#/ProjectsCodes/Aula 3 - Estrutura de decisão IF & ELSE/Combustível - Com IF & ELSE/Program.cs
@@ -1,38 +1,46 @@
 ﻿
-Console.WriteLine("=============================POSTO================================ ");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=============================POSTO================================ ");
 
-Console.WriteLine("Digite a quantidade de litros disponíveis de combustível no posto: ");
-double? estoque = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a quantidade de litros disponíveis de combustível no posto: ");
+        double? estoque = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Digite a cotação em reais por litro: ");
-double? valorLitros = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a cotação em reais por litro: ");
+        double? valorLitros = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("============================CLIENTE=============================== ");
+        Console.WriteLine("============================CLIENTE=============================== ");
 
-Console.WriteLine("Digite seu nome: ");
-String? nomeCliente = (Console.ReadLine());
+        Console.WriteLine("Digite seu nome: ");
+        String? nomeCliente = (Console.ReadLine());
 
-Console.WriteLine($"Olá, seja bem vindo {nomeCliente}. Digite a quantidade de litros desejado: ");
-double? qtdCombustivel = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"Olá, seja bem vindo {nomeCliente}. Digite a quantidade de litros desejado: ");
+        double? qtdCombustivel = Convert.ToDouble(Console.ReadLine());
 
-if (qtdCombustivel > estoque){
-    Console.WriteLine($"Olá, {nomeCliente}. A quantidade solicitada {qtdCombustivel} litros é maior que a disponível em nosso estoque atual, que é de: {estoque} LTS");
-}
-else{
-Console.WriteLine("============================LÓGICA================================ =");
+        if (qtdCombustivel > estoque)
+        {
+            Console.WriteLine($"Olá, {nomeCliente}. A quantidade solicitada {qtdCombustivel} litros é maior que a disponível em nosso estoque atual, que é de: {estoque} LTS");
+        }
+        else
+        {
+            Console.WriteLine("============================LÓGICA================================ =");
 
-    double? valorResultante = valorLitros * qtdCombustivel;
-    double? finalEstoque = estoque - qtdCombustivel;
-    double? combustivelCliente = qtdCombustivel / valorLitros;
+            double? valorResultante = valorLitros * qtdCombustivel;
+            double? finalEstoque = estoque - qtdCombustivel;
+            double? combustivelCliente = qtdCombustivel / valorLitros;
 
-Console.WriteLine("============================SAÍDA================================= =");
+            Console.WriteLine("============================SAÍDA================================= =");
 
-Console.WriteLine(
-$"""
+            Console.WriteLine(
+            $"""
     {nomeCliente} você colocou: {qtdCombustivel} LTS, e o valor total foi de R$: {valorResultante},00.
 
     Estoque atual do posto é de: {finalEstoque} LTS
 """);
 
-Console.WriteLine("================================================================== ");
+            Console.WriteLine("================================================================== ");
+        }
+    }
 }
---
+++ C#/ProjectsCodes/Aula 4 - Estrutura de decisão/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program{
+    static void Main(string[] args)
+    {
+            Console.WriteLine("================================================================== ");
+
+            Console.WriteLine("Bom dia! Digite o seu nome, caro investidor: \n");
+            string? nomeInvestidor = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine($"Olá, {nomeInvestidor} por gentileza digite o saldo de sua conta: ");
+            double? saldo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("============================LÓGICA================================ =");
+
+            double? desconto = (saldo / 100) * 0.05;
+            // Console.WriteLine(desconto);
+
+            double? valorFinal = saldo - desconto;
+            // Console.WriteLine(valorFinal);
+
+            Console.WriteLine("============================SAÍDA================================= =");
+
+            Console.WriteLine("Deseja sacar o dinheiro? [S/N]");
+            string? opc = Convert.ToString(Console.ReadLine());
+
+            if(opc == "s" || opc == "S"){
+
+                Console.WriteLine("Coloque o valor do saque: ");
+                double? saldoInvestidor = Convert.ToDouble(Console.ReadLine());
+
+                if(saldoInvestidor > valorFinal){
+                    Console.WriteLine("Sua tentativa de saque ultrapassa seu saldo atual! Operação Anulada");
+
+                }else{
+                   double? saldoTotal = saldoInvestidor + valorFinal;
+                        Console.WriteLine($"{nomeInvestidor} Saque atualizado com sucesso! Seu saldo atual é de R$: {saldoTotal}");
+                }
+
+            }else{
+                Console.WriteLine("Operação Finalizada");
+            }
+            Console.WriteLine("================================================================== ");
+    }
+}
