7811659d9c1a48d7b50714e55af37f830aca2bf3
offryan <ryan.gsantos3@gmail.com>
2023-01-20 08:49:42 -0300

    cadastrar e mostrar o cliente

--- C#/ProjectsCodes/Aula 6 - Matrizes/Program.cs
+++ C#/ProjectsCodes/Aula 6 - Matrizes/Program.cs
@@ -1,51 +1,29 @@
-class Program{
-    static void Main(string[] args)
-    {
-
-    ﻿while(true){
-    Console.Clear();
-
-    Console.WriteLine("""
-    ================[ Bem vindo ao programa ]=================
-
-    O que você deseja fazer ?
-    1 - Cadastrar o cliente
-    2 - Ver extrato cliente
-    3 - Crédito em conta
-    4 - Débito em conta
-    5 - Sair do sistema
-    """);
-
-    async Task<List<Cliente>> TodosClientes()
-{
+async Task<List<Cliente>> TodosClientes(){
     return await clienteServico.Persistencia.Todos();
 }
 
-async Task<List<ContaCorrente>> TodosExtratos()
-{
+async Task<List<ContaCorrente>> TodosExtratos(){
     return await contaCorrenteServico.Persistencia.Todos();
 }
 
-while (true)
-{
+while (true){
     Console.Clear();
 
     Console.WriteLine("""
     =================[Seja bem-vindo à empresa Lina]=================
     O que você deseja fazer?
-    1 - Cadastrar o cliente
-    2 - Ver extrato cliente
-    3 - Crédito em conta
-    4 - Retirada
-    5 - Sair do sistema
+    1 - Cadastrar cliente
+    2 - Extrato cliente
+    3 - Créditar
+    4 - Débitar
+    5 - Sair
     """);
 
     var opcao = Console.ReadLine()?.Trim();
     Console.Clear();
     bool sair = false;
 
-    switch (opcao)
-    {
+    switch (opcao){
         case "1":
             Console.Clear();
             await cadastrarCliente();
@@ -69,40 +47,108 @@ while (true)
             Console.WriteLine("Opção inválida");
             break;
     }
-    
-
-    async Task menuCadastraClienteSeNaoExiste()
-{
-    Console.WriteLine("""
-    O que você deseja fazer ?
-    1 - Cadastrar cliente
-    2 - Voltar ao menu
-    3 - Sair do programa
-    """);
-
-    var opcao = Console.ReadLine()?.Trim();
-
-    switch(opcao)
-    {
-        case "1":
-            await cadastrarCliente();
-            break;
-        case "3":
-            break;
-        case "2":
-            System.Environment.Exit(0);
-            break;
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
-    }
-}
 
     if (sair) break;
 }
 
-    Thread.Sleep(4000);
-}
-        Console.WriteLine("============================================");
+async Task mostrarContaCorrente(){
+    Console.Clear();
+
+    var clientes = await TodosClientes();
+    var dadosNoExtrato = await TodosExtratos();
+    if(clientes.Count == 0 || dadosNoExtrato.Count == 0){
+        mensagem("Não existe clientes ou não existe movimentações em conta correte, cadastre o cliente e faça crédito em conta");
+        return;
     }
+
+    var cliente = await capturaCliente();
+
+    var contaCorrenteCliente = await contaCorrenteServico.ExtratoCliente(cliente.Id);
+    Console.Clear();
+    Console.WriteLine("----------------------");
+    foreach(var contaCorrente in contaCorrenteCliente){
+        Console.WriteLine("Data: " + contaCorrente.Data.ToString("dd/MM/yyyy HH:mm:ss"));
+        Console.WriteLine("Valor: " + contaCorrente.Valor);
+        Console.WriteLine("----------------------");
+    }
+
+    Console.WriteLine($"""
+    O valor total da conta do cliente {cliente.Nome} é de:
+    R$ {await contaCorrenteServico.SaldoCliente(cliente.Id, contaCorrenteCliente)}
+    """);
+
+    Console.WriteLine("Digite enter para continuar");
+    Console.ReadLine();
+}
+
+async Task listarClientesCadastrados(){
+
+    if((await TodosClientes()).Count == 0){
+        await menuCadastraClienteSeNaoExiste();
+    }
+
+    await mostrarClientes(false, 0, "===============[ Selecione um cliente da lista ]===================");
+}
+
+async Task mostrarClientes(
+    bool sleep = true,
+    int timerSleep = 2000,
+    string header = "===============[ Lista de clientes ]==================="){
+    Console.Clear();
+    Console.WriteLine(header);
+
+    foreach(var cliente in (await TodosClientes())){
+
+        Console.WriteLine("Id:" + cliente.Id);
+        Console.WriteLine("Nome:" + cliente.Nome);
+        Console.WriteLine("Telefone:" + cliente.Telefone);
+        Console.WriteLine("Email:" + cliente.Email);
+        Console.WriteLine("----------------------------");
+
+        if(sleep){
+            Thread.Sleep(timerSleep);
+            Console.Clear();
+        }
+    }
+}
+
+async Task cadastrarCliente(){
+
+    var id = Guid.NewGuid().ToString();
+
+    Console.WriteLine("Informe o nome do cliente:");
+    var nomeCliente = Console.ReadLine();
+
+    Console.WriteLine($"Informe o telefone do cliente {nomeCliente}: ");
+    var telefone = Console.ReadLine();
+
+    Console.WriteLine($"Informe o email do cliente {nomeCliente}: ");
+    var email = Console.ReadLine();
+
+    if((await TodosClientes()).Count > 0){
+
+        Cliente? cli = (await TodosClientes()).Find(c => c.Telefone == telefone);
+
+        if(cli != null){
+
+            mensagem($"Cliente já cadastrado com este telefone {telefone}, cadastre novamente");
+            await cadastrarCliente();
+        }
+    }
+
+    await clienteServico.Persistencia.Salvar(new Cliente{
+
+        Id = id,
+        Nome = nomeCliente ?? "[Sem Nome]",
+        Telefone = telefone != null ? telefone : "[Sem Telefone]",
+        Email = email ?? "[Sem Email]"
+    });
+    mensagem($""" {nomeCliente} cadastrado com sucesso. """);
+}
+
+void mensagem(string msg){
+
+    Console.Clear();
+    Console.WriteLine(msg);
+    Thread.Sleep(1500);
 }
