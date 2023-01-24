ee6fcc2abfeaa9da41a3a618eb2ccc920b790125
offryan <ryan.gsantos3@gmail.com>
2023-01-24 09:35:14 -0300

    Recursividade

---
+++ C#/ProjectsCodes/Recursividade/Recursividade.cs
@@ -0,0 +1,28 @@
+﻿
+LerArquivos(@"C:\Users\Administrator\Desktop\21-dias-de-CSharp\C#\ProjectsCodes\Recursividade");
+
+static void LerArquivos(string LerArquivos)
+{
+    string arquivoCaminho = @"C:\Users\Administrator\Desktop\21-dias-de-CSharp\C#\ProjectsCodes\Recursividade\Nomes das pessoas" + numeroArquivo + ".txt";
+
+    using (StringReader arq = File.OpenText(arquivo))
+    {
+
+        string linha;
+        while ((linha = arq.ReadLine()) != null)
+        {
+            System.Console.WriteLine(linha);
+        }
+    }
+}
+
+string arquivoCaminho = @"C:\Users\Administrator\Desktop\21-dias-de-CSharp\C#\ProjectsCodes\Recursividade\Nomes das pessoas" + numeroArquivo + ".txt";
+if (File.Exists(arquivoCaminho2))
+{
+    LerArquivos(numeroArquivo + 1);
+}
+
+static void Main(string[] args)
+{
+    LerArquivos();
+}
